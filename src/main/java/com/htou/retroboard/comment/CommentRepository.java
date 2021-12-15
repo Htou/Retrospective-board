6c7e4de37c609a477de370f9a678190f4fabd905
Hichem Touati <hichem.touati@student.hu.nl>
2021-12-15 23:13:35 +0100

    Refactored SQL query

--- src/main/java/com/htou/retroboard/comment/CommentRepository.java
+++ src/main/java/com/htou/retroboard/comment/CommentRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-//    @Query("SELECT c FROM Comment c WHERE year(c.createdDate) = ?1 AND month(c.createdDate) = ?2 AND day(c.createdDate) = ?3")
-//    List<Comment> findByCreatedYearAndMonthAndDay(int year, int month, int day);
+    @Query("SELECT c FROM Comment c WHERE c.createdDate = ?1 AND c.createdDate = ?2 AND c.createdDate = ?3")
+    List<Comment> findByCreatedYearAndMonthAndDay(int year, int month, int day);
 
 }
