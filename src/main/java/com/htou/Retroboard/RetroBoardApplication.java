6e18092c01c9bf09c2cc232c1deb347af9c1d6f7
Hichem Touati <hichem.touati@student.hu.nl>
2021-12-15 03:05:07 +0100

    intialized springboot app

---
+++ src/main/java/com/htou/Retroboard/RetroBoardApplication.java
@@ -0,0 +1,13 @@
+package com.htou.Retroboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroBoardApplication.class, args);
+	}
+
+}
