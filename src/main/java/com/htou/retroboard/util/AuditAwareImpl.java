576dabef26c92e9c1408986ab92ff78f618d239c
Hichem Touati <hichem.touati@student.hu.nl>
2021-12-16 01:29:23 +0100

    added AuditAware implementations

---
+++ src/main/java/com/htou/retroboard/util/AuditAwareImpl.java
@@ -0,0 +1,21 @@
+package com.htou.retroboard.util;
+
+import com.htou.retroboard.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(((User) authentication.getPrincipal()).getUsername());
+    }
+}
